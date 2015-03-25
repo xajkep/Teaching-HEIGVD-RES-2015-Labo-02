79e16819029c1ca37ac7189c1146801e9cbee932
jurporan <jan.purro@heig-vd.ch>
2015-03-25 11:07:34 +0100

    test

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/data/EmptyStoreException.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/data/EmptyStoreException.java
@@ -4,7 +4,7 @@ package ch.heigvd.res.labs.roulette.data;
  * An exception thrown when an operation is invoked and expects data to be
  * available in the data store, but when no data is available
  *
- * @author Olivier Liechti
+ * @author Olivier Liechti, Jan Purro Benoist Wolleb
  */
 public class EmptyStoreException extends Exception {
 
