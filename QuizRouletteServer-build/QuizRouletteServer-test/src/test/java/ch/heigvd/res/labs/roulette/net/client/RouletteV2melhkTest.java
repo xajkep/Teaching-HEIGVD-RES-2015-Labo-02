8f0822e55cb98eb0f8aee3544150e7a43739141a
James Nolan <james.nolan@heig-vd.ch>
2015-03-31 00:35:17 +0200

    Added 12 tests

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2melhkTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2melhkTest.java
@@ -2,7 +2,7 @@ package ch.heigvd.res.labs.roulette.net.client;
 
 import ch.heigvd.res.labs.roulette.data.EmptyStoreException;
 import ch.heigvd.res.labs.roulette.data.Student;
-import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 import ch.heigvd.schoolpulse.TestAuthor;
 import java.io.IOException;
 import org.junit.Test;
@@ -10,91 +10,93 @@ import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
+import java.util.List;
 import java.util.LinkedList;
 
 /**
  * This class contains automated tests to validate the client and the server
- * implementation of the Roulette Protocol (version 1)
- * 
+ * implementation of the Roulette Protocol (version 2)
+ *
  * @author Olivier Liechti
  * @author Mélanie Huck
  * @author James Nolan
  */
-public class RouletteV1melhkTest {
+public class RouletteV2melhkTest {
 
   @Rule
   public ExpectedException exception = ExpectedException.none();
-  
-  @Rule
-  public EphemeralClientServerPair roulettePair = new EphemeralClientServerPair(RouletteV1Protocol.VERSION);
 
+  @Rule
+  public EphemeralClientServerPair roulettePair = new EphemeralClientServerPair(RouletteV2Protocol.VERSION);
+  
   @Test
   @TestAuthor(githubId = {"melhk", "j-nolan"})
-  public void theServerShouldReturnAnUniqueStudentWhenItHasStoredOnlyOneStudent() throws IOException, EmptyStoreException {
-      IRouletteV1Client client = roulettePair.getClient();
-      client.loadStudent("Anakin");
-      Student s = client.pickRandomStudent();
-      assertEquals(s.getFullname(), "Anakin");
+  public void theServerShouldReturnTheCorrectVersionNumber() throws IOException {
+    assertEquals(RouletteV2Protocol.VERSION, roulettePair.getClient().getProtocolVersion());
   }
   
   @Test
   @TestAuthor(githubId = {"melhk", "j-nolan"})
-  public void theClientShouldBeAbleToDisconnect() throws IOException {
-      IRouletteV1Client client = roulettePair.getClient();
-      client.disconnect();
-      assertFalse(client.isConnected());
+  public void listOfStudentsShouldBeEmptyAtStart() throws IOException {
+      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+      assertTrue(client.listStudents().isEmpty());
   }
   
   @Test
   @TestAuthor(githubId = {"melhk", "j-nolan"})
-  public void multipleClientsShouldBeAbleToConnectToTheServer() throws IOException {
-    int port = roulettePair.getServer().getPort();
-    IRouletteV1Client client1  = new RouletteV1ClientImpl();
-    IRouletteV1Client client2 = new RouletteV1ClientImpl();
-    client1.connect("localhost", port);
-    client2.connect("localhost", port);
+  public void itShouldBePossibleToClearTheList() throws IOException {
+      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+      assertEquals(0, client.getNumberOfStudents());
+      client.loadStudent("Mew");
+      assertEquals(1, client.getNumberOfStudents());
+      client.loadStudent("Gnocchi");
+      assertEquals(2, client.getNumberOfStudents());
+      client.clearDataStore();
+      assertEquals(0, client.getNumberOfStudents());
+  }
+  
+  @Test
+  @TestAuthor(githubId = {"melhk", "j-nolan"})
+  public void clientShouldRetrieveTheSameListAsItHasSent() throws IOException {
+      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+      List<Student> list1, list2;
+      
+      list1 = new LinkedList<Student>();
+      
+      list1.add(new Student("Annabelle"));
+      list1.add(new Student("Chocolat-man"));
+      client.loadStudents(list1);
+      
+      list2 = client.listStudents();
+      for (int i = 0; i < list1.size(); i++) {
+          assertTrue(list1.get(i).equals(list2.get(i)));
+      }
+      
+      assertEquals(list1.size(), list2.size());
+  }
+  
+  
+  @Test
+  @TestAuthor(githubId = {"melhk", "j-nolan"})
+  public void theServerShouldSendAnErrorResponseWhenRandomIsCalledAndStoreHasBeenCleared() throws IOException, EmptyStoreException {
+    IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+    client.loadStudent("Superpoisson");
+    client.clearDataStore();
+    exception.expect(EmptyStoreException.class);
+    client.pickRandomStudent();
+  }
+  
+  @Test
+  @TestAuthor(githubId = {"melhk", "j-nolan"})
+  public void theClientShouldBeAbleToRetrieveAnotherClientList() throws IOException {
+    IRouletteV2Client client1  = new RouletteV2ClientImpl();
+    IRouletteV2Client client2 = new RouletteV2ClientImpl();
+    client1.loadStudent("Max Poulet");
+    client1.loadStudent("Ventriloque népalais");
     client1.disconnect();
-    client2.disconnect();
+    assertEquals(2, client2.listStudents().size());
   }
+
+
   
-  @Test
-  @TestAuthor(githubId = {"melhk", "j-nolan"})
-  public void aClientShouldBeAbleToReceiveAStudentThatHasBeenStoreByAnotherClient() throws IOException, EmptyStoreException {
-    int port = roulettePair.getServer().getPort();
-    IRouletteV1Client client1  = new RouletteV1ClientImpl();
-    IRouletteV1Client client2 = new RouletteV1ClientImpl();
-    client1.connect("localhost", port);
-    client1.loadStudent("Platon");
-    client1.disconnect();
-    client2.connect("localhost", port);
-    Student s = client2.pickRandomStudent();
-    assertEquals(s.getFullname(), "Platin");
-    client2.disconnect();
-  }
-  
-  @Test
-  @TestAuthor(githubId = {"melhk", "j-nolan"})
-  public void theServerShouldManageStudentsThatHaveTheSameName() throws IOException {
-    String student = "Xénophon";
-    IRouletteV1Client client = roulettePair.getClient();
-    assertEquals(0, client.getNumberOfStudents());
-    client.loadStudent(student);
-    assertEquals(1, client.getNumberOfStudents());
-    client.loadStudent(student);
-    assertEquals(2, client.getNumberOfStudents());
-  }
-  
-  @Test
-  @TestAuthor(githubId = {"melhk", "j-nolan"})
-  public void theServerShouldManageListOfStudentsWithDuplicates() throws IOException {
-    LinkedList<Student> students = new LinkedList<Student>();
-    IRouletteV1Client client = roulettePair.getClient();
-    
-    students.add(new Student("Priam"));
-    students.add(new Student("Cassandre"));
-    students.add(new Student("Cassandre"));
-    client.loadStudents(students);
-    assertEquals(3, client.getNumberOfStudents());
-    
-  }
 }
