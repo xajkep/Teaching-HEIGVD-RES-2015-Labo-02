c20eda7ec9af4914814e4e47d46f0e6cca25c6f3
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2015-04-01 16:27:12 +0200

    Revert "Revert "Merge branch 'fb-lab02-tests' of git://github.com/ond9/Teaching-HEIGVD-RES-2015-Labo-02 into fb-students-tests-validated""
    This reverts commit 8a51c9f788679e1b04675efe5e04f170007832d1.

---
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ond9IIIIIIIIIIIIITest.java
@@ -0,0 +1,123 @@
+package ch.heigvd.res.labs.roulette.net.client;
+
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
+import ch.heigvd.res.labs.roulette.data.Student;
+import ch.heigvd.schoolpulse.TestAuthor;
+import java.io.IOException;
+import java.net.Socket;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import java.io.*;
+import java.util.List;
+import java.util.LinkedList;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This class contains automated tests to validate the client and the server
+ * implementation of the Roulette Protocol (version 1)
+ *
+ * @author Cyrill Zundler and Rick Wertenbroek
+ */
+public class RouletteV1ond9IIIIIIIIIIIIITest {
+
+    final static int BUFFER_SIZE = 1024;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Rule
+    public EphemeralClientServerPair roulettePair = new EphemeralClientServerPair(RouletteV1Protocol.VERSION);
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void RouletteServerExpectedResponseToHelp() throws IOException {
+
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+
+        Socket s = new Socket("localhost", roulettePair.getServer().getPort());
+
+        reader = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
+        writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), "UTF-8"));
+
+        writer.println("HELP");
+        writer.flush();
+
+        String response = reader.readLine();
+
+        String expectedResponse = "Commands: [HELP, RANDOM, LOAD, INFO, BYE]";
+        assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void NumberOfStudentShouldBeCorrect() throws IOException {
+        assertEquals(roulettePair.getClient().getNumberOfStudents(), 0);
+        roulettePair.getClient().loadStudent("farfadet");
+        assertEquals(roulettePair.getClient().getNumberOfStudents(), 1);
+        List<Student> students = new LinkedList<>();
+        students.add(new Student("Cici"));
+        students.add(new Student("Rick"));
+        roulettePair.getClient().loadStudents(students);
+        assertEquals(roulettePair.getClient().getNumberOfStudents(), 3);
+    }
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void RouletteServerExpectedResponseToInfo() throws IOException {
+
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+
+        Socket s = new Socket("localhost", roulettePair.getServer().getPort());
+
+        Integer numberOfStudent = roulettePair.getClient().getNumberOfStudents();
+
+        reader = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
+        writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), "UTF-8"));
+
+        writer.println("INFO");
+        writer.flush();
+
+        String response = reader.readLine();
+
+        String expectedResponse = "{\"protocolVersion\":\"1.0\",\"numberOfStudents\":" + numberOfStudent.toString() + "}";
+        assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void theTestRouletteServerShouldAcceptTwoClientsEachLoadingAStudent() throws IOException {
+        int port = roulettePair.getServer().getPort();
+        IRouletteV1Client client1 = new RouletteV1ClientImpl();
+        IRouletteV1Client client2 = new RouletteV1ClientImpl();
+        client1.connect("localhost", port);
+        client2.connect("localhost", port);
+
+        client1.loadStudent("Laurent");
+        assertEquals(1, client2.getNumberOfStudents());
+        client2.loadStudent("Fafardet");
+
+        assertEquals(2, client1.getNumberOfStudents());
+    }
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void theClientShouldBeDisconnectedAfterDisconnect() throws IOException {
+        IRouletteV1Client client = roulettePair.getClient();
+        assertEquals(true, client.isConnected()); // Client should be connected (connected by Pair)
+        client.disconnect();
+        assertEquals(false, client.isConnected()); // Client should be disconnected
+    }
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void theClientShouldNotBeAbleToLoadAfterDisconnect() throws IOException {
+        IRouletteV1Client client = roulettePair.getClient();
+        client.disconnect();
+        exception.expect(IOException.class);
+        client.loadStudent("Etudiant Fantôme"); // Should throw IOException !
+    }
+
+}
---
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ond9IIIIIIIIIIIIITest.java
@@ -0,0 +1,146 @@
+package ch.heigvd.res.labs.roulette.net.client;
+
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
+import ch.heigvd.res.labs.roulette.data.Student;
+import ch.heigvd.schoolpulse.TestAuthor;
+import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
+import java.util.Vector;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+/**
+ * This class contains automated tests to validate the client and the server
+ * implementation of the Roulette Protocol (version 1)
+ *
+ * @author Cyrill Zundler and Rick Wertenbroek
+ */
+public class RouletteV2ond9IIIIIIIIIIIIITest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Rule
+    public EphemeralClientServerPair roulettePair = new EphemeralClientServerPair(RouletteV2Protocol.VERSION);
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void theServerShouldBeEmptyAfterClear() throws IOException {
+        IRouletteV2Client client = (IRouletteV2Client) roulettePair.getClient();
+        client.loadStudent("Ben Qui Râme"); // On ajouter un student afin qu'il y aie au moins un student
+        client.clearDataStore(); // On vide le serveur
+        assertEquals(0, client.getNumberOfStudents()); // Le serveur devrait être vide
+    }
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void theClientShouldHaveTheSameListOfStudentsAsServer() throws IOException {
+        IRouletteV2Client client = (IRouletteV2Client) roulettePair.getClient();
+        Vector<Student> students = new Vector<>(); // Students to be sent to the server
+        Vector<String> namesClient = new Vector<>();
+        Vector<String> namesServer = new Vector<>();
+        List<Student> studentsReceived; // Students received by the server
+
+        // Creation of a test batch of students
+        for (int i = 0; i < 4; ++i) {
+            students.add(new Student("Number " + (i + 1)));
+            namesClient.add(students.elementAt(i).getFullname());
+        }
+
+        // Load the batch onto the server
+        client.loadStudents(students);
+
+        // We should now get the same students
+        studentsReceived = client.listStudents();
+        for (Student st : studentsReceived) {
+            namesServer.add(st.getFullname());
+        }
+
+        namesClient.sort(null);
+        namesServer.sort(null);
+
+        // We will check this by testing their names
+        assertArrayEquals(namesClient.toArray(), namesServer.toArray());
+    }
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void theServerShouldReturnTheCorrectVersionNumber() throws IOException {
+        assertEquals(RouletteV2Protocol.VERSION, roulettePair.getClient().getProtocolVersion());
+    }
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void theServerShouldRespondCorrectlyOnRandomAfterClear() throws IOException {
+
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+
+        Socket s = new Socket("localhost", roulettePair.getServer().getPort());
+
+        reader = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
+        writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), "UTF-8"));
+
+        writer.println("CLEAR");
+        writer.flush();
+
+        writer.println("RANDOM");
+        writer.flush();
+
+        String response = reader.readLine();
+
+        String expectedResponse = "{\"error\":\"There is no student, you cannot pick a random one\"}";
+        assertEquals(expectedResponse, response);
+
+    }
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void theServerShouldClearTheDataStore() throws IOException {
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+
+        Socket s = new Socket("localhost", roulettePair.getServer().getPort());
+
+        reader = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
+        writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), "UTF-8"));
+
+        writer.println("CLEAR");
+        writer.flush();
+
+        writer.println("INFO");
+        writer.flush();
+
+        String response = reader.readLine();
+
+        String expectedResponse = "{\"protocolVersion\":\"2.0\",\"numberOfStudents\":0}";
+        assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    @TestAuthor(githubId = {"ond9", "IIIIIIIIIIIII"})
+    public void theServerShouldReturnTheCorrectNumberOfCommands() throws IOException {
+
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+
+        Socket s = new Socket("localhost", roulettePair.getServer().getPort());
+
+        reader = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
+        writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), "UTF-8"));
+
+        writer.println("BYE");
+        writer.flush();
+
+        String response = reader.readLine();
+
+        String expectedResponse = "{\"status\":\"success\",\"numberOfCommands\":1}";
+        assertEquals(expectedResponse, response);
+
+    }
+
+}
