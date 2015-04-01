cbb10c0f7de885512fd2a08a8da2be66a50483e9
Benoist Wolleb <planetben1@gmail.com>
2015-04-01 11:20:47 +0200

    Revert "test"
    This reverts commit 79e16819029c1ca37ac7189c1146801e9cbee932.

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/data/EmptyStoreException.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/data/EmptyStoreException.java
@@ -4,7 +4,7 @@ package ch.heigvd.res.labs.roulette.data;
  * An exception thrown when an operation is invoked and expects data to be
  * available in the data store, but when no data is available
  *
- * @author Olivier Liechti, Jan Purro Benoist Wolleb
+ * @author Olivier Liechti
  */
 public class EmptyStoreException extends Exception {
 
